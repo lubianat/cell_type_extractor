edcc0cec5d1bb154ce28b1266ccedf87968c33b0
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2023-02-01 10:34:42 -0300

    feat: Refactor extractor into function

--- src/extractor.py
+++ src/extractor.py
@@ -3,26 +3,38 @@ import openai
 import requests
 from login import API_KEY
 
-openai.api_key = API_KEY
-id = "MED/36104476/"
 
-r = requests.get(
-    f"https://www.ebi.ac.uk/europepmc/webservices/rest/article/{id}?resultType=core&format=json"
-)
+def main():
+    id = "MED/36104476/"
 
-data = r.json()
+    r = requests.get(
+        f"https://www.ebi.ac.uk/europepmc/webservices/rest/article/{id}?resultType=core&format=json"
+    )
 
-abstract_text = data["result"]["abstractText"]
-print(abstract_text)
-response = openai.Completion.create(
-    model="text-davinci-003",
-    prompt=f'Extract all cell mentions in the following text:\n Text: "{abstract_text}" Cell types:',
-    temperature=1,
-    max_tokens=60,
-    top_p=1.0,
-    frequency_penalty=0.0,
-    presence_penalty=0.0,
-    stop=["\n"],
-)
+    data = r.json()
 
-print([a.strip() for a in response["choices"][0]["text"].split(",")])
+    abstract_text = data["result"]["abstractText"]
+    print(abstract_text)
+    cells = extract_cells_from_text(text=abstract_text, API_KEY=API_KEY)
+    print(cells)
+
+
+def extract_cells_from_text(text, API_KEY):
+    openai.api_key = API_KEY
+
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=f'Extract all cell types present in the following text:\n Text: "{text}" Cell types:',
+        temperature=1.0,
+        max_tokens=60,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+        stop=["\n"],
+    )
+
+    return [a.strip() for a in response["choices"][0]["text"].split(",")]
+
+
+if __name__ == "__main__":
+    main()
